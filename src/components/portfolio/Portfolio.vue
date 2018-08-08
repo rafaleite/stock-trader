f3a739b711c0cbea720cd190df9ae89833054f6c
Unknown <rafael@rleite.com.br>
2018-08-08 00:59:01 -0300

    initial config

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>The Portfolio Component</h1>
+</template>
+
+<script>
+    export default {
+        name: "Portfolio"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/stock/Stocks.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>The Stocks Component</h1>
+</template>
+
+<script>
+    export default {
+        name: "Stocks"
+    }
+</script>
+
+<style scoped>
+
+</style>
